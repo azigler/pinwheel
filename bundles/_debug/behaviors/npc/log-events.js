9fe53a7b2ec3cece5db36f29059994a53d8dfadf
Andrew Zigler <andrewzigler@gmail.com>
2018-08-04 20:56:03 -0700

    Rename debug bundle
    - expand debugging logging NPC behavior to include all events not logged by the room
    - remove default debug commands for now due to incomplete and untested functionality

---
+++ bundles/_debug/behaviors/npc/log-events.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Log NPC events to console for debugging purposes.
+ */
+module.exports = (srcPath) => {
+  const Logger = require(srcPath + 'Logger');
+
+  return  {
+    listeners: {
+      playerEnter: state => function (config, player) {
+        Logger.log(`${this.name} saw ${player.name} enter ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      playerLeave: state => function (config, player, destination) {
+        Logger.log(`${this.name} saw ${player.name} leave ${this.room.title} (${this.room.entityReference}) toward ${destination.title} (${destination.entityReference})`);
+      },
+
+      playerDropItem: state => function(config, player, item) {
+        Logger.log(`${this.name} saw ${player.name} drop ${item.name} in ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      combatStart: state => function(config) {
+        Logger.log(`${this.name} started combat.`);
+      },
+
+      combatEnd: state => function(config) {
+        Logger.log(`${this.name} finished combat.`);
+      },
+
+      hit: state => function(config, damage, target) {
+        Logger.log(`${this.name} hit ${target.name} for ${damage.finalAmount} ${damage.attribute} in ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      damaged: state => function (config, damage) {
+        Logger.log(`${this.name} was damaged ${damage.finalAmount} ${damage.attribute} by ${damage.attacker.name} in ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      heal: state => function(config, heal, target) {
+        Logger.log(`${this.name} healed ${target.name} for ${heal.finalAmount} ${heal.attribute} in ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      healed: state => function (config, heal) {
+        Logger.log(`${this.name} was healed ${heal.finalAmount} ${heal.attribute} by ${heal.attacker.name} in ${this.room.title} (${this.room.entityReference})`);
+      },
+
+      npcLeave: state => function (config, target, destination) {
+        if (target.uuid === this.uuid) return;
+        Logger.log(`${this.name} saw ${target.name} leave ${this.room.title} toward ${destination.title} (${destination.entityReference})`);
+      },
+
+      npcEnter: state => function (config, target) {
+        if (target.uuid === this.uuid) return;
+        Logger.log(`${this.name} saw ${target.name} enter ${this.room.title} (${this.room.entityReference})`);
+      },
+    }
+  };
+};
