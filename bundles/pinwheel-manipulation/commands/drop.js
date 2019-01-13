8a5934833dd9454ae555fb9c8b37ca1e29694389
Andrew Zigler <andrewzigler@gmail.com>
2019-01-12 19:07:29 -0800

    Add drop and remove commands

---
+++ bundles/pinwheel-manipulation/commands/drop.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/**
+ * Drop an item
+ * 
+ * @param {string}    args       Arguments provided for the command
+ * @param {Character} character  Character dropping the item
+ * @fires Character#drop
+ * @fires Item#drop
+ * @fires Npc#dropItem
+ */
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const Parser = require(srcPath + 'CommandParser');
+  const ItemUtil = require(srcPath + 'Util/ItemUtil');
+
+  return {
+    usage: 'drop <item>',
+    command: (state) => (args, character) => {
+      args = args.trim();
+
+      // stop if no argument was provided
+      if (!args.length) {
+        return Broadcast.sayAt(character, 'Drop what?');
+      }
+
+      // stop if the character is not in a room
+      if (!character.room) {
+        return Broadcast.sayAt(character, 'You are unable to drop anything here.');
+      }
+
+      // if character sent "drop all"
+      if (args === 'all') {
+        // if they have items, drop them
+        if (character.inventory !== null && character.inventory.size > 0) {
+          character.inventory.forEach(function(value, key, map) {
+            dropItem(value, character);
+          });
+        // otherwise, report there's nothing to drop
+        } else {
+          return Broadcast.sayAt(character, "You aren't carrying anything.");
+        }
+      // otherwise, if they didn't send "drop all"
+      } else {
+        // determine the item to drop
+        const item = Parser.parseDot(args, character.inventory);
+        dropItem(item, character);
+      }
+
+      // helper function for dropping an individual item
+      function dropItem(item, character) {
+        // stop if no matching item was found
+        if (!item) {
+          return Broadcast.sayAt(character, "You aren't carrying that.");
+        }
+
+        // remove the item from the character's inventory and add it to the room
+        character.removeItem(item);
+        character.room.addItem(item);
+
+        /**
+         * @event Character#drop
+         */
+        character.emit('drop', item);
+        
+        /**
+         * @event Item#drop
+         */
+        item.emit('drop', character);
+
+        // notify all NPCs in the room
+        for (const npc of character.room.npcs) {
+          /**
+           * @event Npc#dropItem
+           */
+          npc.emit('dropItem', character, item);
+        }
+
+        // report success
+        Broadcast.sayAt(character, `<b><white>You drop</white></b> ${ItemUtil.display(item)}<b><white>.</white></b>`);
+        Broadcast.sayAtExcept(character.room, `${character.name} drops ${ItemUtil.display(item)}.`, character);
+      }
+    }
+  };
+};
---
+++ bundles/pinwheel-manipulation/commands/remove.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Unequip an item from a character and move it to its inventory
+ * 
+ * @param {string}    args       Arguments provided for the command
+ * @param {Character} character  Character equipping the item
+ */
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const Parser = require(srcPath + 'CommandParser');
+  const ItemUtil = require(srcPath + 'Util/ItemUtil');
+
+  return {
+    aliases: [ 'unwield', 'unequip', 'unhold' ],
+    usage: 'remove <item>',
+    command : state => (arg, character) => {
+      arg = arg.trim();
+
+      // stop if no argument was provided
+      if (!arg.length) {
+        return Broadcast.sayAt(character, 'Remove what?');
+      }
+
+      // determine the item and its slot from the character's equipment
+      const result =  Parser.parseDot(arg, character.equipment, true);
+
+      // stop if the character isn't wearing a matching item
+      if (!result) {
+        return Broadcast.sayAt(character, "You aren't wearing that.");
+      }
+
+      // assign the slot and item from the parse
+      const [slot, item] = result;
+
+      // unequip the item
+      // TIP: this function fires all the needed events
+      character.unequip(slot);
+
+      // report success
+      Broadcast.sayAt(character, `<b><white>You unequip</white></b> ${ItemUtil.display(item)}<b><white>.</white></b>`);
+      Broadcast.sayAtExcept(character.room, Broadcast.capitalize(`${character.name} unequips ${ItemUtil.display(item)}.`), [character]);
+    }
+  };
+};
