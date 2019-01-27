04f7c5e88da35898db78e91dacf828869e7528d9
Andrew Zigler <andrewzigler@gmail.com>
2019-01-26 21:25:19 -0800

    Update player events for Grapevine

--- bundles/pinwheel-communication/player-events.js
+++ bundles/pinwheel-communication/player-events.js
@@ -4,17 +4,21 @@ module.exports = srcPath => {
   return {
     listeners: {
       /**
-       * Report the player to the Gossip network upon login
+       * Report the player to the Grapevine network upon login
        */
       login: state => function() {
-        state.GossipEmitter.emit('players/sign-in', this);
+        if (state.GrapevineEmitter) {
+          state.GrapevineEmitter.emit('players/sign-in', this);
+        }
       },
 
       /**
-       * Report the player to the Gossip network upon disconnecting
+       * Report the player to the Grapevine network upon disconnecting
        */
       quit: state => function() {
-        state.GossipEmitter.emit('players/sign-out', this);
+        if (state.GrapevineEmitter) {
+          state.GrapevineEmitter.emit('players/sign-out', this);
+        }
       },
     }
   };
